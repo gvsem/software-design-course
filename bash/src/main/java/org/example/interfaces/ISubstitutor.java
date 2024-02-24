2393d418c7ef8d904d52cb463c46713e45cd3739
VanjaRo <vanjaro43@gmail.com>
2024-02-24 18:01:35 +0300

    feat: implemented basic functionality of substitutor

---
+++ bash/src/main/java/org/example/interfaces/ISubstitutor.java
@@ -0,0 +1,9 @@
+package org.example.interfaces;
+
+import org.example.ast.concrete.ResolvedCommandExpression;
+import org.example.ast.concrete.UnresolvedCommandExpression;
+import org.example.execution.context.Context;
+
+public interface ISubstitutor {
+    ResolvedCommandExpression resolve(UnresolvedCommandExpression expression, Context ctx);
+}
---
+++ bash/src/test/java/SubstitutorTest.java
@@ -0,0 +1,30 @@
+
+import org.example.ast.concrete.ResolvedCommandExpression;
+import org.example.ast.concrete.UnresolvedCommandExpression;
+import org.example.substitution.Substitutor;
+import org.junit.jupiter.api.Test;
+
+public class SubstitutorTest {
+
+    @Test
+    void substitutionWithSingleAndDoubleQuotes() {
+        // Currently, no Context support
+        Substitutor subs = new Substitutor();
+        String[][] inputAndOutput = {
+                { "echo 'dude \"cat\"'", "echo 'dude \"cat\"'" },
+                // If we meet single quotes with no content, we eliminate them
+                { "echo ''\"\"''", "echo" },
+                { "echo 'dude'\"dude\"'dude'", "echo 'dude''dude''dude'" },
+
+        };
+        for (String[] expectedIo : inputAndOutput) {
+            UnresolvedCommandExpression expr = new UnresolvedCommandExpression(expectedIo[0]);
+            ResolvedCommandExpression resExpr = subs.resolve(expr, null);
+
+            System.out.println(resExpr.getCommand());
+            assert (resExpr.getCommand().equals(expectedIo[1]));
+        }
+
+    }
+
+}
