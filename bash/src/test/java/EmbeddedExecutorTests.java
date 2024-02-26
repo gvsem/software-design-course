a18aa09e99b5c91f6560a685524d24c9d2104022
Georgii Semenov <themrgeorgeous@gmail.com>
2024-02-26 14:33:37 +0300

    deleted legacy test

--- bash/src/test/java/EmbeddedExecutorTests.java
+++ bash/src/test/java/EmbeddedExecutorTests.java
@@ -10,20 +10,20 @@ import util.MockOutputStream;
 
 public class EmbeddedExecutorTests {
 
-    @Test
-    void baselineTest() throws Exception {
-
-        try (MockOutputStream mockOutputStream = new MockOutputStream()) {
-            Command command = new EmbeddedCommand("echo", List.of("5"), List.of());
-            Context context = new Context();
-
-            Executor executor = new Executor();
-            int code = executor.executeProcess(command, context);
-
-            assert(code == 1);
-            mockOutputStream.ensureEquals("Can not run echo");
-        }
-
-    }
+//    @Test
+//    void baselineTest() throws Exception {
+//
+//        try (MockOutputStream mockOutputStream = new MockOutputStream()) {
+//            Command command = new EmbeddedCommand("echo", List.of("5"), List.of());
+//            Context context = new Context();
+//
+//            Executor executor = new Executor();
+//            int code = executor.executeProcess(command, context);
+//
+//            assert(code == 1);
+//            mockOutputStream.ensureEquals("Can not run echo");
+//        }
+//
+//    }
 
 }
