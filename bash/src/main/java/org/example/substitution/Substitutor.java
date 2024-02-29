764d721a497c280799f85719646e82f41cb124f4
Georgii Semenov <themrgeorgeous@gmail.com>
2024-03-01 01:34:17 +0300

    added substitutor

--- bash/src/main/java/org/example/substitution/Substitutor.java
+++ bash/src/main/java/org/example/substitution/Substitutor.java
@@ -15,76 +15,81 @@ import java.util.List;
 
 public class Substitutor implements ISubstitutor {
 
+    private static EnvVariableToken parseEnvironmentToken(String token) {
+        List<String> parts = List.of(token.split("="));
+        if (parts.size() < 2) {
+            throw new IllegalArgumentException("parser tried to interpret it as a env token");
+        }
+        String varName = token.substring(0, parts.get(0).length());
+        String expression = token.substring(parts.get(0).length() + 1);
+
+        StringBuilder t = new StringBuilder();
+        boolean openDoubleQuote = false;
+        boolean openSingleQuote = false;
+
+        for (int i = 0; i < expression.length(); i++) {
+            char x = expression.charAt(i);
+            if (x == ' ' && !openDoubleQuote && !openSingleQuote) {
+                break;
+            } else if (x == '\'' && !openDoubleQuote) {
+                openSingleQuote ^= true;
+            } else if (x == '"' && !openSingleQuote) {
+                openDoubleQuote ^= true;
+            } else {
+                t.append(x);
+            }
+        }
+
+        return new EnvVariableToken(varName, t.toString());
+    }
+
     @Override
     public ResolvedCommandExpression resolve(UnresolvedCommandExpression expression, Context context) {
 
-        List<AbstractToken> tokens = List.of(expression.getCommand().split(" ")).stream()
-                .filter((x) -> !x.isEmpty() && !x.isBlank())
-                .map((x) -> {
-                    if (x.contains("=")) {
-                        List<String> parts = List.of(x.split("="));
-                        if (parts.size() >= 2) {
-                            return new EnvVariableToken(parts.get(0), parts.get(1));
-                        } else {
-                            return new EnvVariableToken(parts.get(0), "");
-                        }
+        List<AbstractToken> tokens = new ArrayList<>();
+
+        StringBuilder token = new StringBuilder();
+        int startPos = 0;
+
+        boolean openDoubleQuote = false;
+        boolean openSingleQuote = false;
+
+        boolean canBeEnvironmentToken = true;
+
+        for (int i = 0; i < expression.getCommand().length(); i++) {
+            char x = expression.getCommand().charAt(i);
+            if (x == ' ' && !openDoubleQuote && !openSingleQuote) {
+                if (token.length() > 0) {
+                    String s = token.toString();
+                    if (canBeEnvironmentToken && s.matches("^[A-Za-z0-9]*=.*")) {
+                        tokens.add(parseEnvironmentToken(expression.getCommand().substring(startPos, i)));
                     } else {
-                        return new StringToken(x);
+                        canBeEnvironmentToken = false;
+                        tokens.add(new StringToken(s));
                     }
-                }).toList();
+                    token.setLength(0);
+                    startPos = i + 1;
+                }
+            } else if (x == '\'' && !openDoubleQuote) {
+                openSingleQuote ^= true;
+            } else if (x == '"' && !openSingleQuote) {
+                openDoubleQuote ^= true;
+            } else {
+                token.append(x);
+            }
+        }
+
+        if (token.length() > 0) {
+            String s = token.toString();
+            if (canBeEnvironmentToken && s.matches("^[A-Za-z0-9]*=.*")) {
+                tokens.add(parseEnvironmentToken(expression.getCommand().substring(startPos)));
+            } else {
+                tokens.add(new StringToken(s));
+            }
+        }
 
         return new ResolvedCommandExpression(tokens);
 
-
-//        StringBuilder resolvedExpression = new StringBuilder();
-//        String originalExpression = expression.getCommand();
-//
-//        boolean inSingleQuote = false;
-//        boolean inDoubleQuote = false;
-//        // points on the last symbol or the last opened quote
-//        int startIndex = 0;
-//
-//        for (int i = 0; i < originalExpression.length(); i++) {
-//            char currentChar = originalExpression.charAt(i);
-//            if (currentChar == '\'' && !inDoubleQuote) {
-//                // Handle single-quoted string:
-//                if (inSingleQuote) {
-//                    // We want to keep the single quote sign
-//                    String resolvedQuote = originalExpression.substring(startIndex, i + 1);
-//                    // Here single quotes are counted
-//                    if (resolvedQuote.length() > 2) {
-//                        resolvedExpression.append(originalExpression, startIndex, i + 1);
-//                    }
-//                    startIndex = i + 1;
-//                }
-//                inSingleQuote = !inSingleQuote;
-//            } else if (currentChar == '"' && !inSingleQuote) {
-//                // Handle double-quoted string:
-//                if (inDoubleQuote) {
-//                    // Perform substitution
-//                    String substituted = substitute(originalExpression.substring(startIndex + 1, i), context);
-//                    if (substituted.length() > 0) {
-//                        // Cover into single quotes
-//                        resolvedExpression.append('\'').append(substituted).append('\'');
-//                    }
-//                    startIndex = i + 1;
-//                }
-//                inDoubleQuote = !inDoubleQuote;
-//            } else if (!inSingleQuote && !inDoubleQuote) {
-//                // Not in quotes, append character
-//                resolvedExpression.append(currentChar);
-//                startIndex = i + 1;
-//            }
-//            // TODO: add cases for substitution outside the quotations
-//
-//        }
-//        return new ResolvedCommandExpression(resolvedExpression.toString().trim());
-
     }
-//
-//    private static String substitute(String token, Context context) {
-//        // Identify all potential substitution places
-//        return token;
-//    }
 
 }
--- bash/src/test/java/SubstitutorTests.java
+++ bash/src/test/java/SubstitutorTests.java
@@ -1,13 +1,65 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.example.ast.concrete.ResolvedCommandExpression;
 import org.example.ast.concrete.UnresolvedCommandExpression;
+import org.example.ast.concrete.token.AbstractToken;
+import org.example.ast.concrete.token.EnvVariableToken;
+import org.example.ast.concrete.token.StringToken;
+import org.example.execution.context.Context;
+import org.example.interfaces.ISubstitutor;
 import org.example.substitution.Substitutor;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 public class SubstitutorTests {
+
+    void checkRightSubstitution(List<AbstractToken> expected, List<AbstractToken> actual) {
+        assertEquals(expected.size(), actual.size());
+        Iterator<AbstractToken> i = expected.iterator(), j = actual.iterator();
+        while (i.hasNext()) {
+            AbstractToken a = i.next();
+            AbstractToken b = j.next();
+            if (a instanceof EnvVariableToken && b instanceof EnvVariableToken) {
+                assertEquals(((EnvVariableToken) a).getVariableName(), ((EnvVariableToken) b).getVariableName());
+                assertEquals(((EnvVariableToken) a).getValue(), ((EnvVariableToken) b).getValue());
+            } else if (a instanceof StringToken && b instanceof  StringToken) {
+                assertEquals(((StringToken) a).getValue(), ((StringToken) b).getValue());
+            } else {
+                assertEquals(1, 0,"Different types");
+            }
+        }
+    }
 
     @Test
     void substitutionWithSingleAndDoubleQuotes() {
+
+        ISubstitutor s = new Substitutor();
+        Context context = new Context();
+
+        checkRightSubstitution(List.of(
+                new EnvVariableToken("LOL", "123"),
+                new EnvVariableToken("KEK", "ke k'11 23'2'3'"),
+                new StringToken("echo"),
+                new StringToken("5"),
+                new StringToken("123'222'222"),
+                new StringToken("1232"),
+                new StringToken("1\"1"),
+                new StringToken("22")
+                ), s.resolve(new UnresolvedCommandExpression(
+                        "LOL=123 KEK=\"ke k'1\"\"1 23'2'3'\" echo 5 \"123'222'222\" '1232' '1\"1' '2''2'"
+        ), context).getTokens());
+
+        checkRightSubstitution(List.of(
+                new EnvVariableToken("LOL", "123"),
+                new EnvVariableToken("KEK", "ke k'11 23'2'3'")
+        ), s.resolve(new UnresolvedCommandExpression(
+                "LOL=123 KEK=\"ke k'1\"\"1 23'2'3'\""
+        ), context).getTokens());
+
 //        // Currently, no Context support
 //        Substitutor subs = new Substitutor();
 //        String[][] inputAndOutput = {
